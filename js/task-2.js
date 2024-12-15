29e2d10ea71e3006b988ffaa33b5d333568d57db
Furkan Sonmez <frkn.snmz46@gmail.com>
2024-12-16 00:58:52 +0300

    task 2 complated

--- js/task-2.js
+++ js/task-2.js
@@ -1,8 +1,25 @@
+class Storage {
+  #items;
 
+  constructor(initialItems) {
+    this.#items = initialItems;
+  }
 
+  getItems() {
+    return this.#items;
+  }
 
+  addItem(newItem) {
+    this.#items.push(newItem);
+  }
+
+  removeItem(itemToRemove) {
+    this.#items = this.#items.filter(item => item !== itemToRemove);
+  }
+}
 
 const storage = new Storage(['Nanitoids', 'Prolonger', 'Antigravitator']);
+
 console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
 storage.addItem('Droid');
 console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
